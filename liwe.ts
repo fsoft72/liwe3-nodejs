e110a280a7b28146c01d41abfc637b4db86ad2eb
Fabio Rotondo <fabio.rotondo@gmail.com>
2021-06-21 15:44:49 +0200

    ADD: relative_fullpath()

--- liwe.ts
+++ liwe.ts
@@ -1,53 +1,61 @@
 import * as fs from './fs';
 
-export const fsname = (path: string): string => {
-	if (!path.startsWith('.')) return fs.abspath('./' + path);
+export const fsname = ( path: string ): string => {
+	if ( !path.startsWith( '.' ) ) return fs.abspath( './' + path );
 
-	return fs.abspath(path);
+	return fs.abspath( path );
 };
 
-export const config_fullpath = (fname: string = ''): string => fsname(`etc/config/${fname}`);
-export const template_fullpath = (modname: string, fname: string = ''): string => fsname(`etc/templates/${modname}/${fname}`);
-export const upload_fullpath = (subpath: string = ''): string => fsname(`static/uploads/${subpath}`);
-export const public_fullpath = (subpath: string = ''): string => fsname(`static/public/${subpath}`);
-export const temp_fullpath = (subpath: string = ''): string => fsname(`static/temp/${subpath}`);
-export const server_fullpath = (subpath: string = ''): string => fsname(`dist/server/${subpath}`);
-export const module_fullpath = (subpath: string = ''): string => fsname(`dist/server/modules/${subpath}`);
+export const config_fullpath = ( fname: string = '' ): string => fsname( `etc/config/${ fname }` );
+export const template_fullpath = ( modname: string, fname: string = '' ): string => fsname( `etc/templates/${ modname }/${ fname }` );
+export const upload_fullpath = ( subpath: string = '' ): string => fsname( `static/uploads/${ subpath }` );
+export const public_fullpath = ( subpath: string = '' ): string => fsname( `static/public/${ subpath }` );
+export const temp_fullpath = ( subpath: string = '' ): string => fsname( `static/temp/${ subpath }` );
+export const server_fullpath = ( subpath: string = '' ): string => fsname( `dist/server/${ subpath }` );
+export const module_fullpath = ( subpath: string = '' ): string => fsname( `dist/server/modules/${ subpath }` );
+export const relative_fullpath = ( fullpath: string = '' ): string => {
+	const fsn = fsname( '.' );
+	const x = fullpath.indexOf( fsn );
 
-export const config_load = (fname: string = '', _default: any = {}, show_error: boolean = false, raise_exception: boolean = false, path: string = 'etc/config'): any => {
-	let name = fsname(`${path}/${fname}.json`);
+	if ( x == -1 ) return fullpath;
 
-	if (!fs.exists(name)) name = fsname(`${path}/${fname}.js`);
+	return fullpath.slice( fsn.length );
+};
 
-	if (!fs.exists(name)) {
-		if (show_error) console.error('ERROR: config not found: ', name);
+export const config_load = ( fname: string = '', _default: any = {}, show_error: boolean = false, raise_exception: boolean = false, path: string = 'etc/config' ): any => {
+	let name = fsname( `${ path }/${ fname }.json` );
 
-		if (raise_exception) throw new Error('ERROR: config not found: ' + name);
+	if ( !fs.exists( name ) ) name = fsname( `${ path }/${ fname }.js` );
+
+	if ( !fs.exists( name ) ) {
+		if ( show_error ) console.error( 'ERROR: config not found: ', name );
+
+		if ( raise_exception ) throw new Error( 'ERROR: config not found: ' + name );
 
 		return _default;
 	}
 
-	return require(name);
+	return require( name );
 };
 
-export const make_default_dirs = (fullpath: string): void => {
-	if (!fs.exists(fullpath)) fs.mkdir(fullpath, 0o755, true);
+export const make_default_dirs = ( fullpath: string ): void => {
+	if ( !fs.exists( fullpath ) ) fs.mkdir( fullpath, 0o755, true );
 };
 
-export const callback_load = (modulename: string, cback_name: string, default_cback: any, report_errors: boolean = true) => {
-	const fname = fsname(`etc/callbacks/${modulename}.js`);
+export const callback_load = ( modulename: string, cback_name: string, default_cback: any, report_errors: boolean = true ) => {
+	const fname = fsname( `etc/callbacks/${ modulename }.js` );
 
-	if (!fs.exists(fname)) {
-		if (report_errors) console.warn(`WARN: missing callbacks for: ${modulename} (${fname})`);
+	if ( !fs.exists( fname ) ) {
+		if ( report_errors ) console.warn( `WARN: missing callbacks for: ${ modulename } (${ fname })` );
 		return default_cback;
 	}
 
 	// eslint-disable-next-line @typescript-eslint/no-var-requires
-	const mod = require(fname);
-	const fn = mod[cback_name];
+	const mod = require( fname );
+	const fn = mod[ cback_name ];
 
-	if (!fn) {
-		if (report_errors) console.warn(`WARN: callback ${cback_name} not found for module ${modulename}`);
+	if ( !fn ) {
+		if ( report_errors ) console.warn( `WARN: callback ${ cback_name } not found for module ${ modulename }` );
 		return default_cback;
 	}
 
@@ -55,23 +63,23 @@ export const callback_load = (modulename: string, cback_name: string, default_cb
 };
 
 export const $l = {
-	str_valid: (t: string): boolean => {
-		if (typeof t !== 'string') return false;
-		if (!t || !t.length) return false;
+	str_valid: ( t: string ): boolean => {
+		if ( typeof t !== 'string' ) return false;
+		if ( !t || !t.length ) return false;
 
 		return true;
 	},
 
-	is_empty: (t: any): boolean => {
-		if (typeof t === 'object') return Object.keys(t).length === 0;
+	is_empty: ( t: any ): boolean => {
+		if ( typeof t === 'object' ) return Object.keys( t ).length === 0;
 
-		if (typeof t === 'string') return t.length === 0;
+		if ( typeof t === 'string' ) return t.length === 0;
 
 		return false;
 	},
 
-	has_key: (t: any, key: string): boolean => {
+	has_key: ( t: any, key: string ): boolean => {
 		// console.log("KEYS: ", Object.keys(t));
-		return Object.keys(t).indexOf(key) !== -1;
+		return Object.keys( t ).indexOf( key ) !== -1;
 	}
 };
