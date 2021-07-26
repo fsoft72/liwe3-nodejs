a2b77df99b1a7036e7e25c81fb4bc37bdc3aa23b
Fabio Rotondo <fabio.rotondo@gmail.com>
2021-07-26 08:30:34 +0200

    FIX when no files are submitted

--- upload.ts
+++ upload.ts
@@ -16,6 +16,8 @@ export const upload_move = ( req: ILRequest, field_name: string, dest_path: stri
 };
 
 export const upload_info = ( req: ILRequest, field_name: string ) => {
+	if ( !req.files ) return {};
+
 	const file: any = req.files[ field_name ];
 
 	if ( !file ) return {};
