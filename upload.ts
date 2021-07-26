09267608b897b66d2b7b2b994a6e3224f9afbaef
Fabio Rotondo <fabio.rotondo@gmail.com>
2021-07-26 07:44:01 +0200

    new upload utils

--- upload.ts
+++ upload.ts
@@ -1,16 +1,30 @@
-// import * as multer from 'multer';
-const multer = require( 'multer' );
+import { mkid } from "./arangodb";
+import { mkdir, move } from "./fs";
+import { ILRequest } from "./types";
 
-import { upload_fullpath } from "./liwe";
+export const upload_move = ( req: ILRequest, field_name: string, dest_path: string, dest_fname?: string ) => {
+	const finfo = upload_info( req, field_name );
 
-const storage = multer.diskStorage( {
-	destination ( req: Express.Request, file: any, cb: any ) {
-		cb( null, upload_fullpath( "temp" ) );
-	},
+	if ( !finfo.size ) return {};
 
-	filename ( req: Express.Request, file: any, cb: any ) {
-		cb( null, Date.now().toString() );
-	}
-} );
+	mkdir( dest_path );
 
-export const upload = multer( { dest: upload_fullpath( "temp" ), storage } );
+	if ( !dest_fname ) dest_fname = mkid( 'file' ) + "." + finfo.ext;
+	move( finfo.path, dest_path + "/" + dest_fname );
+
+	return { path: dest_path, name: dest_fname };
+};
+
+export const upload_info = ( req: ILRequest, field_name: string ) => {
+	const file: any = req.files[ field_name ];
+
+	if ( !file ) return {};
+
+	return {
+		path: file.path,
+		size: file.size,
+		type: file.type,
+		name: file.name,
+		ext: file.name.split( "." ).slice( -1 )
+	};
+};
