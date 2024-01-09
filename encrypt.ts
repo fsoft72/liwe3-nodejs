1bd3444c0918a38d2a4d3bc89d87801c78803be8
Fabio Rotondo <fabio.rotondo@gmail.com>
2024-01-09 15:38:26 +0100

    Add encryption and decryption functions

---
+++ encrypt.ts
@@ -0,0 +1,65 @@
+import * as crypto from 'crypto';
+import * as fs from 'fs';
+import { pipeline } from 'stream/promises';
+
+const algorithm = 'aes-512-cbc';
+
+type encryptKeys = { key: Buffer; iv: Buffer; };
+
+// Returns predictable key and iv based on baseKey
+const buildKey = ( baseKey: string ): encryptKeys => {
+	const key = crypto.createHash( 'sha256' ).update( baseKey ).digest();
+	const iv = crypto.createHash( 'sha256' ).update( key ).digest();
+
+	return { key, iv };
+};
+
+export const encryptBinary = ( data: Buffer, secret: string ): Buffer => {
+	const { key, iv } = buildKey( secret );
+	const cipher = crypto.createCipheriv( algorithm, key, iv );
+	const encrypted = Buffer.concat( [ cipher.update( data ), cipher.final() ] );
+	return encrypted;
+};
+
+export const decryptBinary = ( encryptedData: Buffer, secret: string ): Buffer => {
+	const { key, iv } = buildKey( secret );
+	const decipher = crypto.createDecipheriv( algorithm, key, iv );
+	const decrypted = Buffer.concat( [ decipher.update( encryptedData ), decipher.final() ] );
+	return decrypted;
+};
+
+export const encryptString = ( data: string, secret: string ): string => {
+	const { key, iv } = buildKey( secret );
+	const cipher = crypto.createCipheriv( algorithm, key, iv );
+	const encrypted = Buffer.concat( [ cipher.update( data ), cipher.final() ] );
+	return encrypted.toString( 'hex' );
+};
+
+export const decryptString = ( encryptedData: string, secret: string ): string => {
+	const { key, iv } = buildKey( secret );
+	const decipher = crypto.createDecipheriv( algorithm, key, iv );
+	const decrypted = Buffer.concat( [ decipher.update( encryptedData, 'hex' ), decipher.final() ] );
+	return decrypted.toString();
+};
+
+export const encryptFile = async ( input: string, output: string, secret: string ): Promise<void> => {
+	const { key, iv } = buildKey( secret );
+	const cipher = crypto.createCipheriv( algorithm, key, iv );
+
+	const input_stream = fs.createReadStream( input );
+	const output_stream = fs.createWriteStream( output );
+
+	await pipeline( input_stream, cipher, output_stream );
+};
+
+export const decryptFile = async ( input: string, output: string, secret: string ): Promise<void> => {
+	const { key, iv } = buildKey( secret );
+	const decipher = crypto.createDecipheriv( algorithm, key, iv );
+
+	const input_stream = fs.createReadStream( input );
+	const output_stream = fs.createWriteStream( output );
+
+	await pipeline( input_stream, decipher, output_stream );
+};
+
+
