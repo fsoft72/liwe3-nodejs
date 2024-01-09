626e076210b28d9e410a8b27c3caf731efded405
Fabio Rotondo <fabio.rotondo@gmail.com>
2024-01-09 15:17:26 +0100

    Add LiWEEventResponse types and update liwe_event_emit function

--- events.ts
+++ events.ts
@@ -1,8 +1,11 @@
 import { colors, info, warn } from './console_colors';
 import { ILRequest } from './types';
 
-type LiWEEventHandler = ( req: ILRequest, data: any ) => Promise<void>;
+type LiWEEventHandler = ( req: ILRequest, data: any ) => Promise<LiWEEventSingleResponse>;
 const event_handlers: Record<NamedCurve, LiWEEventHandler[]> = {};
+
+export type LiWEEventSingleResponse = { [ key: string ]: any; };
+export type LiWEEventResponse = LiWEEventSingleResponse[];
 
 export const liwe_event_register = ( modname: string, event: string, handler: LiWEEventHandler ) => {
 	event = event.toLowerCase().trim();
@@ -14,17 +17,23 @@ export const liwe_event_register = ( modname: string, event: string, handler: Li
 };
 
 export const liwe_event_emit = async ( req: ILRequest, event: string, data: any ) => {
+	const response: LiWEEventResponse = [];
+
 	event = event.toLowerCase().trim();
 
 	console.log( "=== Event EMIT: ", event, data );
 
 	const handlers = event_handlers[ event ];
-	if ( !handlers ) return;
+	if ( !handlers ) return response;
 
 	for ( let i = 0; i < handlers.length; i++ ) {
 		const handler = handlers[ i ];
-		await handler( req, data );
+		const res = await handler( req, data );
+
+		if ( res ) response.push( res );
 	}
+
+	return response;
 };
 
 export const liwe_event_unregister = ( event: string, handler: LiWEEventHandler ) => {
